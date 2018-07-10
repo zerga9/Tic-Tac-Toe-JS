9b65da6b797c834243e0537353c0010555addcce
Gadiza Zerari <gadizaz@hotmail.com>
2018-07-10 15:06:37 +0100

    Test there are 2 players passing

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -2,10 +2,14 @@ describe("Game", function(){
   var game;
 
   beforeEach(function() {
-    game = new Game();
+    game = new Game("player1", "player2");
   });
 
   it("should give 9 empty places", function(){
     expect(game.grid).toEqual(["", "", "", "", "", "", "", "", ""]);
   });
+  it("should have 2 players", function(){
+    expect(game.player1).toEqual("player1")
+    expect(game.player2).toEqual("player2")
+  })
 })
--- src/Game.js
+++ src/Game.js
@@ -1,3 +1,5 @@
-function Game(){
+function Game(player1 = new Player, player2 = new Player){
     this.grid = ["", "", "", "", "", "", "", "", ""];
+    this.player1 = player1
+    this.player2 = player2
 }
