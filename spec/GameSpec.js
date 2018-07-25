bd6ed49bf3c14d68d3e867f42b1278ebc2651013
Gadiza Zerari <gadizaz@hotmail.com>
2018-07-25 14:37:44 +0100

    Change grid to have blank space

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,25 +1,24 @@
-describe("Game", function(){
+describe("Game", function() {
   var game;
   var player;
 
   beforeEach(function() {
-
-    player1 = new Player("X")
-    player2 = new Player("Y")
+    player1 = new Player("X");
+    player2 = new Player("O");
     game = new Game(player1, player2);
   });
 
-  it("should give 9 empty places", function(){
-    expect(game.grid).toEqual(["", "", "", "", "", "", "", "", ""]);
+  it("should give 9 empty places", function() {
+    expect(game.grid).toEqual([" ", " ", " ", " ", " ", " ", " ", " ", " "]);
   });
 
-  it("should have 2 players", function(){
-    expect(game.player1).toEqual(player1)
-    expect(game.player2).toEqual(player2)
-  })
+  it("should have 2 players", function() {
+    expect(game.player1).toEqual(player1);
+    expect(game.player2).toEqual(player2);
+  });
 
-  it("should let the player choose a position and change it in the players' token", function(){
-   game.play(player1, 2)
-   expect(game.grid).toEqual(["", "", "X", "", "", "", "", "", ""])
-   })
-})
+  it("should let the player choose a position and change it in the players' token", function() {
+    game.play(player1, 2);
+    expect(game.grid).toEqual([" ", " ", "X", " ", " ", " ", " ", " ", " "]);
+  });
+});
--- src/Game.js
+++ src/Game.js
@@ -1,8 +1,13 @@
-function Game(player1 = new Player("X"), player2 = new Player("O")){
-    this.grid = ["", "", "", "", "", "", "", "", ""];
-    this.player1 = player1
-    this.player2 = player2
+function Game(player1 = new Player("X"), player2 = new Player("O")) {
+  this.grid = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+  this.player1 = player1;
+  this.player2 = player2;
 }
-Game.prototype.play = function(player, position){
-  this.grid[position] = player.token
-}
+Game.prototype.play = function(player, position) {
+  this.grid[position] = player.token;
+};
+Game.prototype.display = function() {
+  console.log(this.grid[0] + this.grid[1] + this.grid[2]);
+  console.log(this.grid[3] + this.grid[4] + this.grid[5]);
+  console.log(this.grid[6] + this.grid[7] + this.grid[8]);
+};
